6767116c99b0183fd60abff44ac114de0225ed31
Nida Nurrohmah <60107104@udst.edu.qa>
2025-02-04 19:10:10 +0300

    Fix: email input validation

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Email, ValidationError
 
 class ContactForm(FlaskForm):
     name = StringField('Name')
     phone = StringField('Phone')
-    email = StringField('Email')
+    email = StringField('Email', validators=[DataRequired(), Email()])
     type = SelectField('Type', 
                       choices=[('Personal', 'Personal'), 
                               ('Work', 'Work'), 
