3ac5e956fb7c3e90163f637bd0361b8a7c6047d6
Student <student@10233-01.local>
2025-02-05 10:39:10 +0300

    feature-validatedcontact

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired, Length, Regexp
 class ContactForm(FlaskForm):
     first = StringField('First Name',validators=[DataRequired(), Length(min=1, max=50),Regexp(r'^[A-Za-z\s]+$', message="First name must only contain letters and spaces.")])
     last = StringField('Last Name', validators=[DataRequired(),Length(min=1, max=50),Regexp(r'^[A-Za-z\s]+$', message="Last name must only contain letters and spaces.")])
-    phone = StringField('Phone')
+    phone = StringField('Phone', validators=[DataRequired(),Length(min=1,max=8, message="Phone number must be exactly 8 characters long."),Regexp(r'^\d{8}$', message="Phone number must only contain 8 digits.")])
     email = StringField('Email', validators=[DataRequired(), Email()])
     type = SelectField('Type', 
                       choices=[('Personal', 'Personal'), 
