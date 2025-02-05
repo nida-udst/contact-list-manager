96f730a1b1200615ecc02672b86fc2089737a049
Student <student@10233-01.local>
2025-02-05 10:21:52 +0300

    feature-validatedfirstandlastname

--- forms.py
+++ forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length, Regexp
 
 class ContactForm(FlaskForm):
-    first = StringField('first')
-    last = StringField('last')
+    first = StringField('First Name',validators=[DataRequired(), Length(min=1, max=50),Regexp(r'^[A-Za-z\s]+$', message="First name must only contain letters and spaces.")])
+    last = StringField('Last Name', validators=[DataRequired(),Length(min=1, max=50),Regexp(r'^[A-Za-z\s]+$', message="Last name must only contain letters and spaces.")])
     phone = StringField('Phone')
     email = StringField('Email', validators=[DataRequired(), Email()])
     type = SelectField('Type', 
